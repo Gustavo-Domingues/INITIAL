0d6b8689b6795533f193e869b2703ba8ae785b42
Gustavo Domingues <65620456+Gustavo-Domingues@users.noreply.github.com>
2022-04-26 23:13:47 -0300

    First

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+* Start Bootstrap - Business Frontpage v5.0.8 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+function Mudarestado(divid){
+    if( document.getElementById(divid).style.display === "none"){
+        document.getElementById(divid).style ="lead text-white-50 mb-4";
+    }else{
+        document.getElementById(divid).style.display ="none";
+    }}   
